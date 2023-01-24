44465cc24d12e1ccfb6d2e25cf9c8c032e79d981
wildanmujahidin <wildan.mujahidin1@gmail.com>
2023-01-24 14:12:20 +0700

    fetch: add Layout, Header and Navbar component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Hearder = () => {
+  return (
+    <div className="py-5 shadow-md ">
+      <h1
+        style={{ fontFamily: "Montserrat Alternates" }}
+        className="font-black text-3xl tracking-wider"
+      >
+        YSHOP
+      </h1>
+    </div>
+  );
+};
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import { Layout } from "../components/Layout";
 
 export const Homepage = () => {
-  return <div>Homepage</div>;
+  return <Layout>Homepage</Layout>;
 };
