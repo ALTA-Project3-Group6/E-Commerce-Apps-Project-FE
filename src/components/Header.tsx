c9d118a44ececa8f9a9bdab6273731fd91d9ecd9
wildanmujahidin <wildan.mujahidin1@gmail.com>
2023-01-24 17:31:49 +0700

    fetch: add Homepage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Hearder = () => {
   return (
-    <div className="py-5 shadow-md ">
+    <div className="py-5 shadow-md drop-shadow-xl ">
       <h1
         style={{ fontFamily: "Montserrat Alternates" }}
         className="font-black text-3xl tracking-wider"
